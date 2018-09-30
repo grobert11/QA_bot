8b1164da4935a60dbf584955468650f2bb25bb46
Robert Garsantsyan <zordon@MacBook-Pro-Robert.local>
2018-09-30 21:37:58 +0300

    fix

--- main.js
+++ main.js
@@ -11,6 +11,17 @@ const uchi  = ['<@U6UHL6Z5F>', '<@U7NCLDEJ3>',];
 const help = ['envi - Окружающий мир', '\nnative - Русский язык', '\nelmath - Математика начальная школа', '\nmidmath - Математика основная школа','\nhighmath - Математика старшая школа', '\neng - Английский язык', '\nuchi - Учи.ру/Олимпиады']
 const channel = 'support';
 
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
 
 // create a bot
 var bot = new SlackBot({
